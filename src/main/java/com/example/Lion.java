f7ff90d525b4d5970aeb9b4178ebb870c74a5b81
nastyaPotato <nasty15012013@gmail.com>
2022-10-30 14:10:14 +0300

    Финальный проект спринта 6

--- src/main/java/com/example/Lion.java
+++ src/main/java/com/example/Lion.java
@@ -6,7 +6,7 @@ public class Lion {
 
     boolean hasMane;
 
-    public Lion(String sex) throws Exception {
+    public Lion(String sex, Feline feline) throws Exception {
         if ("Самец".equals(sex)) {
             hasMane = true;
         } else if ("Самка".equals(sex)) {
@@ -14,9 +14,10 @@ public class Lion {
         } else {
             throw new Exception("Используйте допустимые значения пола животного - самей или самка");
         }
+        this.feline=feline;
     }
 
-    Feline feline = new Feline();
+    private Feline feline;
 
     public int getKittens() {
         return feline.getKittens();
